ba331db460f128f905ca3e688393275e2838723d
Darus112 <edarius123@gmail.com>
2023-03-10 10:01:10 +0200

    forms

--- restaurant_web_app/src/pages/BookTable.js
+++ restaurant_web_app/src/pages/BookTable.js
@@ -1,3 +1,67 @@
+import React from "react";
+import Bounce from 'react-reveal/Bounce';
+
 export default function BookTable() {
-    return <h1>Rezervari</h1>
+    return (
+    <>
+    <section id="form">
+      <Bounce>
+        <div class="container2">
+          <h3 class="form_title">Rezervare</h3>
+          <div class="form_wrapper">
+            <form name="booking" method="POST" netlify>
+              <div class="form_group">
+                <label for="firstName">First Name</label>
+                <input type="text" id="firstName" name="First Name" required/>
+              </div>
+              <div class="form_group">
+                <label for="lastName">Last Name</label>
+                <input type="text" id="lastName" name="Last Name" required/>
+              </div>
+              <div class="form_group">
+                <label for="email">Email</label>
+                <input type="email" id="email" name="Email" required/>
+              </div>
+              <div class="form_group">
+                <label for="tableType">Table Type</label>
+                <select name="Table Type" id="tableType" required>
+                  <option selected disabled>Choose</option>
+                  <option value="small">Small(2 persons)</option>
+                  <option value="medium">Small(4 persons)</option>
+                  <option value="large">large(6 persons)</option>
+                </select>
+              </div>
+              <div class="form_group">
+                <label for="guestNumber">Guest Number</label>
+                <input type="number" id="guestNumber" name="Guest Number" min="1" max="10" required/>
+              </div>
+              <div class="form_group">
+                <label for="placement">Placement</label>
+                <select name="Placement" id="placement">
+                  <option selected disabled>Choose</option>
+                  <option value="outdoor">Outdoor</option>
+                  <option value="indoor">Indoor</option>
+                  <option value="rooftop">rooftop</option>
+                </select>
+              </div>
+              <div class="form_group">
+                <label for="date">Date</label>
+                <input type="date" id="date" name="Date" required/>
+              </div>
+              <div class="form_group">
+                <label for="time">time</label>
+                <input type="time" id="time" name="Time" required/>
+              </div>
+              <div class="form_group form_group_full">
+                <label for="note">Note</label>
+                <textarea name="Note" id="note" rows="4"></textarea>
+              </div>
+              <button type="submit" class="btn-bg">Book Table</button>
+            </form>
+          </div>
+        </div>
+      </Bounce>
+    </section>
+    </>
+    );
   }
--- restaurant_web_app/src/pages/Contact.js
+++ restaurant_web_app/src/pages/Contact.js
@@ -1,3 +1,85 @@
+import React from "react";
+import "./styles/Global.css";
+import Fade from 'react-reveal/Fade';
+import Bounce from 'react-reveal/Bounce';
+
 export default function Contact() {
-    return <h1>Contact</h1>
+    return (
+    <>
+    <section id="storeInfo">
+      <Bounce>
+        <div class="container2">
+          <div class="storeInfo_wrapper">
+            <div class="storeInfo_item">
+              <div class="storeInfo_icon">
+              <i class="fa-solid fa-clock"></i>
+              </div>
+              <h3 class="storeInfo_title">
+                10 AM - 7 PM
+              </h3>
+              <p class="storeInfo_text">
+                Opening Hour
+              </p>
+            </div>
+            <div class="storeInfo_item">
+              <div class="storeInfo_icon">
+              <i class="fa-solid fa-location-dot"></i>
+              </div>
+              <h3 class="storeInfo_title">
+                GEC, Chittagong
+              </h3>
+              <p class="storeInfo_text">
+                Address
+              </p>
+            </div>
+            <div class="storeInfo_item">
+              <div class="storeInfo_icon">
+              <i class="fa-solid fa-phone-volume"></i>
+              </div>
+              <h3 class="storeInfo_title">
+                +880123443
+              </h3>
+              <p class="storeInfo_text">
+                Call Now
+              </p>
+            </div>
+          </div>
+        </div>
+        </Bounce>
+    </section>
+
+    <section id="form">
+      <Fade bottom>
+        <div class="container2">
+          <h3 class="form_title">Contact</h3>
+          <div class="form_wrapper">
+            <form name="contact" method="POST" netlify>
+              <div class="form_group">
+                <label for="firstName">First Name</label>
+                <input type="text" id="firstName" name="First Name" required/>
+              </div>
+              <div class="form_group">
+                <label for="lastName">Last Name</label>
+                <input type="text" id="lastName" name="Last Name" required/>
+              </div>
+              <div class="form_group">
+                <label for="email">Email</label>
+                <input type="email" id="email" name="Email" required/>
+              </div>
+              <div class="form_group">
+                <label for="subject">Subject</label>
+                <input type="text" id="subject" name="Subject" required/>
+              </div>
+              <div class="form_group form_group_full">
+                <label for="message">Message</label>
+                <textarea name="Message" id="message" cols="30" rows="10" required></textarea>
+              </div>
+              <button type="submit" class="btn-bg">Send</button>
+            </form>
+          </div>
+        </div>
+      </Fade>
+    </section>
+    </>
+    );
   }
